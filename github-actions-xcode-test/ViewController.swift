11d831d13248b53edd0344bc575e3d1aa474db42
akidon0000 <a@art.jp>
2023-11-25 13:53:42 +0900

    init

---
+++ github-actions-xcode-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  github-actions-xcode-test
+//
+//  Created by Akihiro Matsuyama on 2023/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
